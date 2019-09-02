9c642da7b703b6d833e205a99d11793c5d743b96
Amirhuusain <meghdadianah@yahoo.com>
2019-09-02 19:13:47 +0430

    fixing an issue

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const css = document.querySelector('h3');
 const body = document.querySelector("body");
 
 setGradient = () =>{
-	body.style.background = _linearGradient();
+	body.style.background = linearGradient();
 	// css.appendChild(document.createTextNode(body.style.background + ";"));
-	
+	css.textContent = `${body.style.background};`;
 }
 
-_linearGradient = () =>
+linearGradient = () =>
 {
-	return `linear-gradient(to right, ${color1.value} , ${color2.value});`;
+	return `linear-gradient(to right, ${color1.value} , ${color2.value})`;
 }
 color1.addEventListener('input',setGradient);
 color2.addEventListener('input',setGradient);
