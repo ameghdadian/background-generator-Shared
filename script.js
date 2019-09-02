3a3577ac7459f9246c673665a4b572a107cfae3c
Amirhuusain <meghdadianah@yahoo.com>
2019-09-02 18:46:46 +0430

    Updating syntax to ES6

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-var color1 = document.querySelector("#color1");
-var color2 = document.querySelector("#color2");
-var h3 = document.querySelector("h3");
-var body = document.querySelector("body");
+// var color1 = document.querySelector("#color1");
+// var color2 = document.querySelector("#color2");
+const [color1 , color2] = document.querySelectorAll('.color');
+// var h3 = document.querySelector("h3");
+// var body = document.querySelector("body");
+const h3 = document.querySelector("h3");
+const body = document.querySelector("body");
 
-function setGradient(){
-	body.style.background = "linear-gradient( to right, "+
-	color1.value + 
-	", " +
-	color2.value +
-	")";
-
-	h3.appendChild(document.createTextNode(body.style.background + ";"));
+setGradient = () =>{
+	body.style.background = _linearGradient();
+	// h3.appendChild(document.createTextNode(body.style.background + ";"));
+	h3.appendChild(document.createTextNode(`${body.style.background} ;`));
 }
 
-
-color1.addEventListener("input",setGradient);
-color2.addEventListener("input",setGradient);
+_linearGradient = () =>
+{
+	return `Linear-Gradient(to right, ${color1.value} , ${color2.value});`;
+}
+color1.addEventListener('input',setGradient);
+color2.addEventListener('input',setGradient);
 
 
 
