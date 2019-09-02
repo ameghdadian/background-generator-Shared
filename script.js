5ace3c1841de4fd7cffec833cd00e345b8940dd8
Amirhuusain <meghdadianah@yahoo.com>
2019-09-02 19:00:34 +0430

    fixing bugs

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // var color1 = document.querySelector("#color1");
 // var color2 = document.querySelector("#color2");
-const [color1 , color2] = document.querySelectorAll('.color');
-// var h3 = document.querySelector("h3");
+// var css = document.querySelector("css");
 // var body = document.querySelector("body");
-const h3 = document.querySelector("h3");
+const [color1 , color2] = document.querySelectorAll('.color');
+const css = document.querySelector('h3');
 const body = document.querySelector("body");
 
 setGradient = () =>{
 	body.style.background = _linearGradient();
-	// h3.appendChild(document.createTextNode(body.style.background + ";"));
-	h3.appendChild(document.createTextNode(`${body.style.background} ;`));
+	// css.appendChild(document.createTextNode(body.style.background + ";"));
+	
 }
 
 _linearGradient = () =>
 {
-	return `Linear-Gradient(to right, ${color1.value} , ${color2.value});`;
+	return `linear-gradient(to right, ${color1.value} , ${color2.value});`;
 }
 color1.addEventListener('input',setGradient);
 color2.addEventListener('input',setGradient);
