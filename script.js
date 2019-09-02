2cffbe3e15e6af1b42014b0574f9ac506cc0703f
Amirhuusain <meghdadianah@yahoo.com>
2019-09-02 20:36:32 +0430

    browserify added

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//Can use this syntax with webpack and ES6
+// import { without } from 'lodash';
+// console.log(without);	
+
+//Here we should use browserify to do the above action
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6,7,8,9];
+console.log('answer:',_.without(array,3));
+
 // var color1 = document.querySelector("#color1");
 // var color2 = document.querySelector("#color2");
 // var css = document.querySelector("css");
